f299281026398296eda53be409aa2316755475ba
Brian Birdwell <bbirdwell@gmail.com>
2018-10-02 05:00:53 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,8 +22,8 @@ def bonus
 
   #code your solution here:
 
-  def bonus(key, value)
-    epic_tragedy[:key]= "dead"
+  def bonus(hash)
+    hash.[]=
   end
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
