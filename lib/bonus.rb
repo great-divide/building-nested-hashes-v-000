3df29cb07bd489294bdcd44dc2138c845005659c
Brian Birdwell <bbirdwell@gmail.com>
2018-10-02 04:42:21 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,7 +23,7 @@ def bonus
   #code your solution here:
 
   def []=(key, value)
-    value = "dead"
+    [:key] = "dead"
   end
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
