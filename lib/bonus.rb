173ef2fa50cb15f6b4c7107216897d7327f04528
Brian Birdwell <bbirdwell@gmail.com>
2018-10-02 04:39:44 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,9 @@ def bonus
 
   #code your solution here:
 
-  def []=
+  def []=(key, value)
+    
+  end
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
